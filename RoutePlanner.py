b69f709f3fb06bcc5a86798a602f0250cd2362b4
camknopp <camknopp@gmail.com>
2020-07-21 20:54:13 -0400

    Some reformatting

--- RoutePlanner.py
+++ RoutePlanner.py
@@ -1,10 +1,10 @@
 from openrouteservice import client, places, directions, distance_matrix
-from ortools.constraint_solver import pywrapcp, routing_enums_pb2
 from geopy.geocoders import Nominatim
 import json
 import folium
 
-api_key = '5b3ce3597851110001cf62484c5ca3f76ff944dfa2d23b773efec8d7'  # api key for openroute service
+# api key for openrouteservice (which will figure out the optimal route)
+api_key = '5b3ce3597851110001cf62484c5ca3f76ff944dfa2d23b773efec8d7'
 clnt = client.Client(key=api_key)
 
 geolocator = Nominatim(user_agent='Meals On Wheels Route')
@@ -12,11 +12,12 @@ geolocator = Nominatim(user_agent='Meals On Wheels Route')
 locs_addresses = ['milford ma', 'boston ma', 'hopkinton ma', 'east boston']
 locs_coords = []
 
-
+# convert above addresses to coordinates using geopy
 for loc in locs_addresses:
-    curr = geolocator.geocode(loc)  # get the coordinates of the above addresses
+    curr = geolocator.geocode(loc) 
     locs_coords.append((curr.longitude, curr.latitude))
 
+# create folium map
 m = folium.Map(location=locs_coords[0], zoom_start=12)
 m.save('map.html')
 
