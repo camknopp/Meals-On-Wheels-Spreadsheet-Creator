2042d69777807bb3b86d3df0c6bd472e8fa58e40
camknopp <camknopp@gmail.com>
2021-01-30 08:59:47 -0500

    Cloned form for various vlaues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,25 +7,62 @@ This app will give an easy to use
 */
 
 function App() {
-  let [showForm, setShowForm] = useState(false)
+	let [showForm, setShowForm] = useState(false)
+
+	const handleSubmit = e => {
+		e.preventDefault()
+		console.log("Submit pressed")
+	}
 
 	return (
 		<div>
 			<h1>Meals on Wheels Spreadsheet Creator</h1>
-			<div onClick={(e) => {
-        e.preventDefault()
-        setShowForm(!showForm)
-      }}>
-        click here
-      </div>
-        
-				<Collapse isOpened={showForm}>
-					<form>
-						Enter data:
-						<input type="text" name="name" />
-					</form>
-				</Collapse>
-			
+			<div
+				onClick={e => {
+					e.preventDefault()
+					setShowForm(!showForm)
+				}}
+			>
+				Click here to add new person
+			</div>
+
+			<Collapse isOpened={showForm}>
+				<form id="NameForm" onSubmit={handleSubmit}>
+					Enter Name:
+					<input type="text" />
+					<button type="submit" form="NameForm">
+						+
+					</button>
+				</form>
+				<form id="StreetAddressForm" onSubmit={handleSubmit}>
+					Enter Street Address:
+					<input type="text" />
+					<button type="submit" form="StreetAddressForm">
+						+
+					</button>
+				</form>
+        <form id="CityTownForm" onSubmit={handleSubmit}>
+					Enter City or Town:
+					<input type="text" />
+					<button type="submit" form="CityTownForm">
+						+
+					</button>
+				</form>
+        <form id="StateForm" onSubmit={handleSubmit}>
+					Enter State:
+					<input type="text" />
+					<button type="submit" form="StateForm">
+						+
+					</button>
+				</form>
+        <form id="MealForm" onSubmit={handleSubmit}>
+					Enter Meal:
+					<input type="text" />
+					<button type="submit" form="MealForm">
+						+
+					</button>
+				</form>
+			</Collapse>
 		</div>
 	)
 }
